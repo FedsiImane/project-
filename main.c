e0f0413ef3fd26b1fce0522fe71423a2ff1c9e38
FedsiImane <iman14092003@gmail.com>
2024-01-07 16:35:33 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  nnn
+//
+//  Created by imannina on 7/1/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
